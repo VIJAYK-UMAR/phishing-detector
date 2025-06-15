de64d4160951ac012466f4bee30cd359ffa8b8bb
VIJAYK-UMAR <147915085+VIJAYK-UMAR@users.noreply.github.com>
2025-06-15 18:48:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ def predict():
         print(f"❌ Error occurred: {e}")
         return jsonify({"error": str(e)})
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 # if __name__ == "__main__":
-#     from waitress import serve
-#     serve(app, host="0.0.0.0", port=8080)
+#     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
