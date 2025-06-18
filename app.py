db54b356f3ee21493eccf10a7ddee5e089ff9edb
VIJAYK-UMAR <147915085+VIJAYK-UMAR@users.noreply.github.com>
2025-06-18 23:37:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
